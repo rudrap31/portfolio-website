5f58fd856e26accf5c30c080321a88dbd9592f78
Rudra Patel <3rudrap1@gmail.com>
2024-05-22 18:51:02 -0600

    Added landing section with 3d model

---
+++ main.js
@@ -0,0 +1,60 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+//typing animation
+
+var typed = new Typed(".typing",{
+    strings : ["Software Engineer", "ubc student", "travel enthusiast"],
+    typeSpeed : 150,
+    backSpeed : 125,
+    loop : true,
+});  
+
+
+//threejs
+var container = document.getElementById('container');
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+
+
+const loader = new GLTFLoader();
+
+loader.load( 'space-3dmodel/scene.gltf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+	console.error( error );
+
+} );
+scene.rotation.y = -1.2;
+camera.position.z = 3;
+camera.position.y = 0.3;
+
+
+
+
+function animate() {
+
+	requestAnimationFrame( animate );
+    scene.rotation.y += 0.0025
+	renderer.render( scene, camera );
+}
+
+animate();
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}); 
